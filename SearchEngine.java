67679088969a098ddbf77acb1b3dd2b9ccd32eb7
Diana Doan <122428383+d2doan@users.noreply.github.com>
2023-04-13 11:52:32 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -7,6 +7,7 @@ public class SearchEngine implements URLHandler{
     
     public String handleRequest(URI url){
         ArrayList<String> str = new ArrayList<>();
+
         if(url.getPath().equals("add")){
             if(url.getQuery().equals("s")){
                 String[] quer = url.getQuery().split("=");
@@ -29,5 +30,18 @@ public class SearchEngine implements URLHandler{
             }
         }
         return "Command does not yet exist, come back later...";
+    }
+}
+
+class NumberServer {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
     }
 }
